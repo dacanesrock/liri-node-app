eb5602e3fcbe6adf669ee9c1cf46fcc733a72d9a
Robert Geyer <Robert Geyer>
2017-04-26 17:41:03 -0400

    now logs to separate txt file

--- liri.js
+++ liri.js
@@ -5,6 +5,7 @@ var input2 = process.argv[3];
 var spotify = require("spotify");
 var request = require("request");
 var fs = require("fs");
+var data = "";
 
 if (input1 === 'my-tweets') {
     myTweets();
@@ -18,11 +19,14 @@ if (input1 === 'my-tweets') {
 
 function myTweets() {
     var client = new Twitter(keys.twitterKeys);
+    fs.appendFileSync("log.txt", "-----------" + "\n" + "method: " + input1 + "\n" + "-----------");
     client.get('search/tweets', { q: 'dacanesrock', count: '20' }, function(err, tweets, response) {
         for (var i = 0; i < tweets.statuses.length; i++) {
-            console.log('Tweet: ' + tweets.statuses[i].text);
-            console.log('Date Created: ' + tweets.statuses[i].created_at);
-            console.log("----------------------------")
+        	data = "Tweet " + (i + 1) +": " + tweets.statuses[i].text + "\n" +
+        	'Date Created: ' + tweets.statuses[i].created_at + "\n" + 
+        	"----------------------------";
+        	console.log(data);
+        	fs.appendFileSync("log.txt", "\n" + data + "\n");
         }
     });
 };
@@ -39,12 +43,15 @@ function spotifyThisSong(input, song) {
             console.log('Error occurred: ' + err);
             return;
         };
-        console.log("----------------")
-        console.log("ARTIST: " + data.tracks.items[0].artists[0].name);
-        console.log("TRACK NAME: " + data.tracks.items[0].name);
-        console.log("PREVIEW LINK: " + data.tracks.items[0].preview_url);
-        console.log("ALBUM: " + data.tracks.items[0].album.name);
-        console.log("----------------")
+        data = 
+        "----------------" + "\n" +
+        "ARTIST: " + data.tracks.items[0].artists[0].name + "\n" +
+        "TRACK NAME: " + data.tracks.items[0].name + "\n" +
+        "PREVIEW LINK: " + data.tracks.items[0].preview_url + "\n" +
+        "ALBUM: " + data.tracks.items[0].album.name + "\n" +
+        "----------------";
+        console.log(data);
+        fs.appendFileSync("log.txt", "-----------" + "\n" + "method: " + input1 + "\n" + data + "\n" + "-----------");
     });
 };
 
@@ -63,24 +70,29 @@ function movieThis() {
             console.log('Error occurred: ' + err);
             return;
         };
-        console.log("----------------")
-        console.log("TITLE: " + JSON.parse(data).Title);
-        console.log("YEAR RELEASED: " + JSON.parse(data).Year);
-        console.log("IMDB RATING: " + JSON.parse(data).Ratings[0].Value);
-        console.log("COUNTRY PRODUCED: " + JSON.parse(data).Country);
-        console.log("LANGUAGE: " + JSON.parse(data).Language);
-        console.log("PLOT: " + JSON.parse(data).Plot);
-        console.log("ACTORS: " + JSON.parse(data).Actors);
-        console.log("ROTTEN TOMATOES RATING: " + JSON.parse(data).Ratings[1].Value);
-        console.log("METASCORE: " + JSON.parse(data).Metascore);
-        console.log("----------------")
+        data = 
+        "----------------" + "\n" + 
+        "TITLE: " + JSON.parse(data).Title + "\n" +
+        "YEAR RELEASED: " + JSON.parse(data).Year + "\n" +
+        "IMDB RATING: " + JSON.parse(data).Ratings[0].Value + "\n" +
+        "COUNTRY PRODUCED: " + JSON.parse(data).Country + "\n" +
+        "LANGUAGE: " + JSON.parse(data).Language + "\n" +
+        "PLOT: " + JSON.parse(data).Plot + "\n" +
+        "ACTORS: " + JSON.parse(data).Actors + "\n" +
+        "ROTTEN TOMATOES RATING: " + JSON.parse(data).Ratings[1].Value + "\n" +
+        "METASCORE: " + JSON.parse(data).Metascore + "\n" +
+        "----------------";
+        console.log(data);
+        fs.appendFileSync("log.txt", "-----------" + "\n" + "method: " + input1 + "\n" + data + "\n" + "-----------");
     });
+
 };
 
 // //////////////////////
 // this is a mess, i know
 function doWhatItSays() {
     fs.readFile('./random.txt', 'utf8', (err, data) => {
+        fs.appendFileSync("log.txt", "-----------" + "\n" + "method: " + input1 + "\n" + data + "\n" + "-----------");    	
         if (err) throw err;
         var dataArray = data.split(",");
         input1 = dataArray[0];
